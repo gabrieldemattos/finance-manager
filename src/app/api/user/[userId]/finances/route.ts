a7c21481e89a967c9bb2d9752e690646c6d8d99f
Gabriel de Mattos <mattos_95@hotmail.com>
2024-03-07 13:57:27 -0300

    refactor: Refactor the function

--- src/app/api/user/[userId]/finances/route.ts
+++ src/app/api/user/[userId]/finances/route.ts
@@ -1,38 +1,28 @@
 import { prismaClient } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(
-  request: NextRequest,
-  { params: { userId } }: { params: { userId: string } },
-) {
-  try {
-    if (!userId) {
-      return new NextResponse(JSON.stringify({ message: "Missing userId" }), {
-        status: 400,
-      });
-    }
-
-    const user = await prismaClient.user.findUnique({
-      where: {
-        id: userId,
-      },
-      include: {
-        finances: true,
-      },
+  request: Request,
+  {
+    params: { userId },
+  }: {
+    params: { userId: string };
+  },
+): Promise<NextResponse> {
+  if (!userId) {
+    return new NextResponse(JSON.stringify({ message: "Missing userId" }), {
+      status: 400,
     });
-
-    if (!user) {
-      return new NextResponse(JSON.stringify({ message: "User not found" }), {
-        status: 404,
-      });
-    }
-
-    return new NextResponse(JSON.stringify(user), { status: 200 });
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    return new NextResponse(
-      JSON.stringify({ message: "Internal server error" }),
-      { status: 500 },
-    );
   }
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      finances: true,
+    },
+  });
+
+  return new NextResponse(JSON.stringify(user), { status: 200 });
 }
